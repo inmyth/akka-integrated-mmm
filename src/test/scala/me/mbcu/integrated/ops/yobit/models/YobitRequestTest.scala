ef6bedd996c6ad25808b0f71f311798db335c0e4
Martin <martin@hanatoya.jp>
2018-07-04 15:23:14 +0900

    Removed YobitRequest gen non-idempotent

--- src/test/scala/me/mbcu/integrated/ops/yobit/models/YobitRequestTest.scala
+++ src/test/scala/me/mbcu/integrated/ops/yobit/models/YobitRequestTest.scala
@@ -6,11 +6,11 @@ import org.scalatest.FunSuite
 class YobitRequestTest extends FunSuite{
   val secret = "sknfskfw3904239sdf233242"
 
-  test("test yobit nonce gen non-idempotent") {
-    val nonce1 = YobitRequest.nonce
-    val nonce2 = YobitRequest.nonce
-    assert(nonce2 > nonce1)
-  }
+//  test("test yobit nonce gen non-idempotent") {
+//    val nonce1 = YobitRequest.nonce
+//    val nonce2 = YobitRequest.nonce
+//    assert(nonce2 > nonce1)
+//  }
 
   test ("test Yobit sha512HMAC sign has to be in lowercase") {
     val test = YobitRequest.ownTrades(secret, "trx_eth")
